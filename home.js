462c3d46ba320c95bdd9d66592397238ca86b961
aShinde <ananta.shinde@honeywellcom>
2023-04-18 09:20:10 +0530

    Initial

---
+++ home.js
@@ -0,0 +1,12 @@
+ const hstruct = `<!DOCTYPE html>
+<html lang="en">
+<head>
+      
+    <title>Home</title>
+</head>
+<body>
+     <H1>Hello , Welcome To Home</H1>
+</body>
+</html>`
+
+module.exports = hstruct;
