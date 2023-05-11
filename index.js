20e24bee428a1af8ae45f1acd541485514fe6cef
aShinde <ananta.shinde@honeywellcom>
2023-05-11 09:22:47 +0530

    temp commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+var cors = require('cors')
 const userRouter = require("./routes/user")
 const productRouter = require("./routes/product")
 const categoryRouter = require("./routes/category")
 const app = express();
 app.use(express.json())
+app.use(cors())
 
  
 app.use("/users",userRouter)
@@ -11,4 +13,4 @@ app.use("/products",productRouter)
 app.use("/categories",categoryRouter)
 
  
-app.listen(3000);
+app.listen(4000);
--- model/product.js
+++ model/product.js
@@ -4,7 +4,8 @@ const productSchema = new db.Schema({
     title : {type : String, required:true},
     description : {type : String, required:true},
     mrp : {type : Number, required:true},
-    category : {type:String,required:true}   
+    category : {type:String,required:true},
+    image :  {type:String,required:true} 
 })
 
 const productModel = new db.model("product",productSchema);
